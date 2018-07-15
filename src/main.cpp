f6e153b1cff84bcc7475cb6f911b7c3f33dae0a5
caijjh <caijh23@gmail.com>
2018-07-15 22:33:26 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@ int main()
     Data::getInstance()->loadTrainDataByColumn();
     cout << "load train data completed" << endl;
     GBDT gbdt;
-    gbdt.setParameters(10,10,0.01,0.01,0.08,0.05);
+    gbdt.setParameters(5,5,0.01,0.01,0.08,0.05);
     gbdt.initModel();
     gbdt.train();
     cout << "load predict data ..." << endl;
     Data::getInstance()->loadPredictData();
     cout << "load predict data completed" << endl;
     float* prediction = gbdt.predict();
-    Data::getInstance()->savePrediction(prediction,"../data/predictionV1.txt");
+    Data::getInstance()->savePrediction(prediction,"../data/predictionV2.txt");
     clock_gettime(CLOCK_REALTIME, &finish);
     cout << "Totle Time : " << (finish.tv_sec + 1.e-9 * finish.tv_nsec) - (start.tv_sec + 1.e-9 * start.tv_nsec) << "s" << endl;
     return 0;
